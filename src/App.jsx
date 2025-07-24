4e9ce4a93379fa48672e6a58b733d9d15009a7fd
Farhan <fnasim1102@gmail.com>
2025-07-24 18:14:15 +0600

    added basic animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { useRef } from "react";
+
 function App() {
+  const box = useRef();
+  useGSAP(() => {
+    gsap.from(box.current, {
+      y: 300,
+      duration: 1,
+      delay: 1,
+      opacity: 0,
+      scale: 0,
+    });
+  });
   return (
     <>
-      <h1 className="text-6xl">hello world</h1>
+      <div
+        className="w-[300px] h-[300px] bg-fuchsia-600 m-auto"
+        ref={box}
+      ></div>
     </>
   );
 }
